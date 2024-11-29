f9dc297b49c719ad875e8d6da5e692fc303a1151
dvp-tae <th3016@naver.com>
2024-11-29 14:22:20 +0000

    hotfix: base 경로 다시 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "@svgr/rollup";
 
-export default defineConfig(({ mode }: { mode: string }) => {
-  const isProduction = mode === "production";
-
-  return {
-    plugins: [react(), svgr()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    define: {
-      global: "window",
-    },
-    base: isProduction ? "/dist/" : "./",
-    build: {
-      outDir: "dist",
-      assetsDir: "assets",
-    },
-  };
+  },
+  define: {
+    global: "window",
+  },
 });
